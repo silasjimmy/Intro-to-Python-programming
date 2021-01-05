888f71b1dae7154785dde52c1fb8e0a3e6b160f9
silas jimmy <jimmysilas17@gmail.com>
2021-01-05 06:09:09 +0300

    ps4c completed

--- ps4/ps4c.py
+++ ps4/ps4c.py
@@ -1,9 +1,8 @@
 # Problem Set 4C
-# Name: <your name here>
-# Collaborators:
-# Time Spent: x:xx
+# Name: Silas Jimmy
+# Collaborators: None
+# Time Spent: 3hrs
 
-import string
 from ps4a import get_permutations
 
 ### HELPER CODE ###
@@ -11,13 +10,8 @@ def load_words(file_name):
     '''
     file_name (string): the name of the file containing 
     the list of words to load    
-    
     Returns: a list of valid words. Words are strings of lowercase letters.
-    
-    Depending on the size of the word list, this function may
-    take a while to finish.
     '''
-    
     print("Loading word list from file...")
     # inFile: file
     inFile = open(file_name, 'r')
@@ -32,28 +26,16 @@ def is_word(word_list, word):
     '''
     Determines if word is a valid word, ignoring
     capitalization and punctuation
-
     word_list (list): list of words in the dictionary.
     word (string): a possible word.
-    
     Returns: True if word is in word_list, False otherwise
-
-    Example:
-    >>> is_word(word_list, 'bat') returns
-    True
-    >>> is_word(word_list, 'asdf') returns
-    False
     '''
     word = word.lower()
     word = word.strip(" !@#$%^&*()-_+={}[]|\:;'<>?,./\"")
     return word in word_list
 
-
-### END HELPER CODE ###
-
 WORDLIST_FILENAME = 'words.txt'
 
-# you may find these constants helpful
 VOWELS_LOWER = 'aeiou'
 VOWELS_UPPER = 'AEIOU'
 CONSONANTS_LOWER = 'bcdfghjklmnpqrstvwxyz'
@@ -63,12 +45,7 @@ class SubMessage(object):
     def __init__(self, text):
         '''
         Initializes a SubMessage object
-                
         text (string): the message's text
-
-        A SubMessage object has two attributes:
-            self.message_text (string, determined by input text)
-            self.valid_words (list, determined using helper function load_words)
         '''
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
@@ -76,16 +53,13 @@ class SubMessage(object):
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-        
         Returns: self.message_text
         '''
         return self.message_text
 
     def get_valid_words(self):
         '''
-        Used to safely access a copy of self.valid_words outside of the class.
-        This helps you avoid accidentally mutating class attributes.
-        
+        Used to safely access a copy of self.valid_words outside of the class.        
         Returns: a COPY of self.valid_words
         '''
         valid_words = self.valid_words[:]
@@ -94,19 +68,6 @@ class SubMessage(object):
     def build_transpose_dict(self, vowels_permutation):
         '''
         vowels_permutation (string): a string containing a permutation of vowels (a, e, i, o, u)
-        
-        Creates a dictionary that can be used to apply a cipher to a letter.
-        The dictionary maps every uppercase and lowercase letter to an
-        uppercase and lowercase letter, respectively. Vowels are shuffled 
-        according to vowels_permutation. The first letter in vowels_permutation 
-        corresponds to a, the second to e, and so on in the order a, e, i, o, u.
-        The consonants remain the same. The dictionary should have 52 
-        keys of all the uppercase letters and all the lowercase letters.
-
-        Example: When input "eaiuo":
-        Mapping is a->e, e->a, i->i, o->u, u->o
-        and "Hello World!" maps to "Hallu Wurld!"
-
         Returns: a dictionary mapping a letter (string) to 
                  another letter (string). 
         '''
@@ -118,7 +79,6 @@ class SubMessage(object):
     def apply_transpose(self, transpose_dict):
         '''
         transpose_dict (dict): a transpose dictionary
-        
         Returns: an encrypted version of the message text, based 
         on the dictionary
         '''
@@ -136,33 +96,15 @@ class EncryptedSubMessage(SubMessage):
     def __init__(self, text):
         '''
         Initializes an EncryptedSubMessage object
-
         text (string): the encrypted message text
-
-        An EncryptedSubMessage object inherits from SubMessage and has two attributes:
-            self.message_text (string, determined by input text)
-            self.valid_words (list, determined using helper function load_words)
         '''
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
 
     def decrypt_message(self):
         '''
-        Attempt to decrypt the encrypted message 
-        
-        Idea is to go through each permutation of the vowels and test it
-        on the encrypted message. For each permutation, check how many
-        words in the decrypted text are valid English words, and return
-        the decrypted message with the most English words.
-        
-        If no good permutations are found (i.e. no permutations result in 
-        at least 1 valid word), return the original string. If there are
-        multiple permutations that yield the maximum number of words, return any
-        one of them.
-
-        Returns: the best decrypted message    
-        
-        Hint: use your function from Part 4A
+        Attempts to decrypt the encrypted message
+        Returns: the best decrypted message
         '''
         permutations = get_permutations(VOWELS_LOWER)
         max_valid_words = 0
